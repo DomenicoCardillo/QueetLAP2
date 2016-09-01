f15907c04e9fa70841df72a6600a24cc877a733f
Marco Nisi <marco@paneedesign.com>
2016-09-01 23:00:24 +0200

    Fixed crash on signup/login with empty fields

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -21,7 +21,9 @@ export default class LoginPage extends Component {
   }
 
   login() {
-    this.props.submit(this.state.email, this.state.pass)
+    if(this.state && this.state.email && this.state.pass) {
+      this.props.submit(this.state.email, this.state.pass)
+    }
   }
   goToForm() {
     Actions.userForm()
