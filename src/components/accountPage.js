8643c6477e5e78b36e9a7658a565d1978d354d62
Domenico <domenico.cardillo.94@gmail.com>
2016-09-03 12:58:20 +0200

    Test ImagePicker

--- src/components/accountPage.js
+++ src/components/accountPage.js
@@ -1,13 +1,18 @@
 import React, {
-  Component
+    Component
 } from 'react'
 
 import {
-  Text,
-  View,
-  StyleSheet,
-  BackAndroid
+    View,
+    Text,
+    StyleSheet,
+    Platform,
+    BackAndroid,
+    TouchableOpacity
 } from 'react-native'
+
+import ImagePicker from 'react-native-image-crop-picker'
+import NativeModules from 'NativeModules'
 
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
@@ -16,9 +21,6 @@ import Button from 'apsl-react-native-button'
 
 import commonStyles from '../styles/commons'
 import styleVariables from '../styles/variables'
-
-const homePlace = {description: 'Home', geometry: { location: { lat: 48.8152937, lng: 2.4597668 } }}
-const workPlace = {description: 'Work', geometry: { location: { lat: 48.8496818, lng: 2.2940881 } }}
 
 export default class AccountPage extends Component {
   constructor (props) {
@@ -30,86 +32,110 @@ export default class AccountPage extends Component {
       return true
     })
 
-    /* Geolocalization */
-    /*navigator.geolocation.getCurrentPosition(
-      (position) => {
-        let initialPosition = JSON.stringify(position)
-        this.setState({initialPosition})
+    /* File Upload (Work on iOS, follow guide for android) */
+    /*let obj = {
+      uploadUrl: 'http://127.0.0.1:3000',
+      method: 'POST', // default 'POST',support 'POST' and 'PUT'
+      headers: {
+        'Accept': 'application/json',
       },
-      (error) => alert(error.message),
-      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-    )
-    navigator.geolocation.watchPosition((position) => {
-      let lastPosition = JSON.stringify(position)
-      this.setState({lastPosition})
-    });*/
+      fields: {
+        'hello': 'world',
+      },
+      files: [
+        {
+          name: 'one', // optional, if none then `filename` is used instead
+          filename: 'one.w4a', // require, file name
+          filepath: '/xxx/one.w4a', // require, file absoluete path
+          filetype: 'audio/x-m4a', // options, if none, will get mimetype from `filepath` extension
+        },
+      ]
+    }
+
+    NativeModules.FileUpload.upload(obj, function(err, result) {
+      console.log('upload:', err, result);
+    })*/
+  }
+
+  openPicker() {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 400,
+      cropping: true
+    }).then(image => {
+      console.log(image);
+    });
   }
 
   render() {
     return (
-      <View style={commonStyles.container}>
-        <GooglePlacesAutocomplete
-          placeholder='Search'
-          minLength={2} // minimum length of text to search
-          onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-            console.log(data);
-            console.log(details);
-          }}
-          getDefaultValue={() => {
-            return ''; // text input default value
-          }}
-          query={{
-            // available options: https://developers.google.com/places/web-service/autocomplete
-            key: 'AIzaSyB8XnrQEsL9JUIo2u4T7xbrvpHxTe39jD4',
-            language: 'it', // language of the results
-            types: '(cities)', // default: 'geocode'
-          }}
-          styles={{
-            description: {
-              fontWeight: 'bold',
-            },
-            predefinedPlacesDescription: {
-              color: '#1faadb',
-            },
-            textInputContainer: {
-              backgroundColor: '#fff',
-              borderWidth: 1,
-              borderTopWidth: 1,
-              borderBottomWidth: 1,
-              borderColor: styleVariables.colors.borderColor,
-              borderTopColor: styleVariables.colors.borderColor,
-              borderBottomColor: styleVariables.colors.borderColor,
-              borderRadius: styleVariables.baseRadius
-            },
-            listView: {
-              borderLeftWidth: 1,
-              borderRightWidth: 1,
-              borderBottomWidth: 1,
-              borderTopWidth: 0,
-              maxHeight: 200,
-              borderColor: styleVariables.colors.borderColor,
-              borderRadius: styleVariables.baseRadius
-            },
-            separator: {
-              backgroundColor: styleVariables.colors.borderColor,
-            }
-          }}
-          enablePoweredByContainer={false}
-          currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
-          currentLocationLabel="Current location"
-          nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-          GoogleReverseGeocodingQuery={{
-            // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-          }}
-          GooglePlacesSearchQuery={{
-            // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-            rankby: 'distance',
-            types: 'food',
-          }}
-          // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-          filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} 
-        />
-      </View>
+        <View style={commonStyles.container}>
+          <GooglePlacesAutocomplete
+              placeholder='Search'
+              minLength={2} // minimum length of text to search
+              onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+                console.log(data)
+                console.log(details)
+              }}
+              getDefaultValue={() => {
+                return '' // text input default value
+              }}
+              query={{
+                // available options: https://developers.google.com/places/web-service/autocomplete
+                key: 'AIzaSyB8XnrQEsL9JUIo2u4T7xbrvpHxTe39jD4',
+                language: 'it', // language of the results
+                types: '(cities)', // default: 'geocode'
+              }}
+              styles={{
+                description: {
+                  fontWeight: 'bold',
+                },
+                predefinedPlacesDescription: {
+                  color: '#1faadb',
+                },
+                textInputContainer: {
+                  backgroundColor: '#fff',
+                  borderWidth: 1,
+                  borderTopWidth: 1,
+                  borderBottomWidth: 1,
+                  borderColor: styleVariables.colors.borderColor,
+                  borderTopColor: styleVariables.colors.borderColor,
+                  borderBottomColor: styleVariables.colors.borderColor,
+                  borderRadius: styleVariables.baseRadius
+                },
+                listView: {
+                  borderLeftWidth: 1,
+                  borderRightWidth: 1,
+                  borderBottomWidth: 1,
+                  borderTopWidth: 0,
+                  maxHeight: 200,
+                  borderColor: styleVariables.colors.borderColor,
+                  borderRadius: styleVariables.baseRadius
+                },
+                separator: {
+                  backgroundColor: styleVariables.colors.borderColor,
+                }
+              }}
+              enablePoweredByContainer={false}
+              currentLocation={false} // Will add a 'Current location' button at the top of the predefined places list
+              currentLocationLabel="Current location"
+              nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+              GoogleReverseGeocodingQuery={{
+                // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+              }}
+              GooglePlacesSearchQuery={{
+                // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+                rankby: 'distance',
+                types: 'food',
+              }}
+              // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+              filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']}
+          />
+
+          <TouchableOpacity onPress={this.openPicker.bind(this)}>
+            <Text>Open picker</Text>
+          </TouchableOpacity>
+        </View>
     )
   }
 }
