f701dca936cc17f7006955c31d8970c45747660f
Marco Nisi <marco@paneedesign.com>
2016-09-26 16:27:37 +0200

    Correct order for users

--- src/containers/users.js
+++ src/containers/users.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux'
 import UsersPage from '../components/usersPage'
 import { Actions } from 'react-native-router-flux'
 import { fetchUsers, listenUsersChanges, setUserDetail, setUsersActiveFilter } from '../actions/users'
-import { filterByFriends } from '../globals'
+import { filterByFriends, sortArrayByProps } from '../globals'
 
 const getVisibleUsers = (users, currentUser, activeFilter) => {
   return filterByFriends(users, currentUser, activeFilter == 'Friends')
@@ -15,6 +15,7 @@ const mapStateToProps = (state) => {
   } else {
     users = getVisibleUsers(state.users, state.auth.currentUser, state.usersPage.activeFilter)
   }
+  sortArrayByProps(users, 'desc', 'id')
   return {
     users,
     isLoading: state.usersPage.isLoading,
